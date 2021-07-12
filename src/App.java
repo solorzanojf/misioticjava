03ad44877c4382ff1f03d7924879b91dee82f839
solorzanojf <solorzanojf@gmail.com>
2021-07-12 18:44:44 -0500

    uso de scanner y calculadora

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         System.out.println("Hello, World!");
+
+        Scanner ingresar=new Scanner(System.in);
+        String nombre = ingresar.nextLine();
+        System.out.println("su nombre es "+nombre);
+        
     }
 }
--- src/calculadora.java
+++ src/calculadora.java
@@ -1,13 +1,35 @@
-public class calculadora {
-    public static void main(String[] args) throws Exception {
-        int numero1= 10;
-        int suma= numero1+5;
-        int resta= numero1-5;
-        int multi= numero1*5;
-        int divi= numero1/5;
-        System.out.println(suma);
-        System.out.println(resta);
-        System.out.println(multi);
-        System.out.println(divi);
-    }
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String args []) throws Exception {
+        System.out.println("PARA SUMA MARQUE 1");
+        System.out.println("PARA RESTA MARQUE 2");
+        System.out.println("PARA MULTIPLICACION MARQUE 3");
+        System.out.println("PARA SUMA DIVICION 4");
+        Scanner scanner = new Scanner(System.in);
+        int operacion= scanner.nextInt();
+        System.out.println("INGRESE EL PRIMER NUMERO ENTERO");
+        int numero1= scanner.nextInt();
+        System.out.println("INGRESE EL SEGUNDO NUMERO ENTERO");
+        int numero2= scanner.nextInt();
+        int resultado= 0;
+        switch (operacion){
+            case 1:
+                resultado = numero1+numero2;
+                break;
+            case 2:
+                resultado = numero1-numero2;
+                break;
+            case 3:
+                resultado = numero1*numero2;
+                break;
+            case 4:
+                resultado = numero1/numero2;
+                break;
+            default:
+                System.out.println("el tipo de operacion no es valido");
+          }
+          System.out.println(resultado);
+        }
+           
 }
