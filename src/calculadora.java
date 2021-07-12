54d51a8c7a0477ccb4d2775d8ef509e809dcb5f7
solorzanojf <solorzanojf@gmail.com>
2021-07-12 12:12:08 -0500

    primer comentario

---
+++ src/calculadora.java
@@ -0,0 +1,13 @@
+public class calculadora {
+    public static void main(String[] args) throws Exception {
+        int numero1= 10;
+        int suma= numero1+5;
+        int resta= numero1-5;
+        int multi= numero1*5;
+        int divi= numero1/5;
+        System.out.println(suma);
+        System.out.println(resta);
+        System.out.println(multi);
+        System.out.println(divi);
+    }
+}
